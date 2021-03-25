c033c2202f7496f08c0c324c6e17c7eaac8df597
Reynelle <Rtalford13@gmail.com>
2021-03-24 21:01:11 -0400

    updated index.erb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,32 @@
 class EntriesController < ApplicationController
     
     get '/posts' do
-        @posts = Post.all
+        @entries = Entry.all
         erb :index
+    end 
+
+    get '/posts/new' do 
+        erb : 'entries/new'
+    end 
+
+    get '/posts/:id' do 
+        @entry = Entry.find(params[:id])
+        erb :'entries/show'
+    end 
+
+    post '/posts' do
+
+    end 
+
+    get 'posts/:id/edit' do
+
+    end 
+
+    patch '/posts/:id' do 
+
+    end 
+
+    delete '/post/:id' do 
+    
     end 
 end
--- db/migrate/02_create_entry.rb
+++ db/migrate/02_create_entry.rb
@@ -1,4 +1,4 @@
-class CreateEntriess < ActiveRecord::Migrations[5.2]
+class CreateEntries < ActiveRecord::Migrations[5.2]
     def change 
         create_table :entries do |t|
             t.string :title 
