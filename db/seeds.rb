46d7476e4c21fc69f4bb270f998ded568af62714
Reynelle <Rtalford13@gmail.com>
2021-04-07 03:00:09 -0400

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 User.create!(email: 'test@test.com', password: 'test')
 Entry.create!(Title: 'first entry', content: "This is my content", user_id: 1)
 Entry.create!(Title: 'second entry', content: "This is additional content", user_id: 2)
+Entry.create!(Title: 'Third entry', content: "This is more additional content", user_id: 3)
